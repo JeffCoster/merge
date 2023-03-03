81b5decfd156bc4332dd25c8147713f1494bf39c
JeffCoster <jeffcoster1@gmail.com>
2023-03-03 13:34:32 +0000

    Added Example 1 Product Lister files

---
+++ ex1/content/product-list-shoes.js
@@ -0,0 +1,103 @@
+export const globalContent = {
+   "pageTitle":"Product Lister",
+   "pageImg":"https://dummyjson.com/image/i/products/57/1.jpg",
+   "sizeLabel":"Sizes",
+};
+
+export const prods = {
+   "products": [
+      {
+         "id": 56,
+         "title": "Sneakers Joggers Shoes",
+         "description": "Gender: Men , Colours: Same as DisplayedCondition: 100% Brand New",
+         "price": 40,
+         "discountPercentage": 12.57,
+         "rating": 4.38,
+         "stock": 6,
+         "brand": "Sneakers",
+         "category": "mens-shoes",
+         "thumbnail": "https://dummyjson.com/image/i/products/56/thumbnail.jpg",
+         "images": [
+            "https://dummyjson.com/image/i/products/56/1.jpg", "https://dummyjson.com/image/i/products/56/2.jpg", "https://dummyjson.com/image/i/products/56/3.jpg", "https://dummyjson.com/image/i/products/56/4.jpg", "https://dummyjson.com/image/i/products/56/5.jpg", "https://dummyjson.com/image/i/products/56/thumbnail.jpg"
+         ],
+         "sizes": [
+            6, 7, 8
+         ]
+      },
+      {
+         "id": 57,
+         "title": "Loafers for men",
+         "description": "Men Shoes - Loafers for men - Rubber Shoes - Nylon Shoes - Shoes for men - Pure Nylon (Rubber) Export Quality.",
+         "price": 47,
+         "discountPercentage": 10.91,
+         "rating": 4.91,
+         "stock": 20,
+         "brand": "Rubber",
+         "category": "mens-shoes",
+         "thumbnail": "https://dummyjson.com/image/i/products/57/thumbnail.jpg",
+         "images": [
+            "https://dummyjson.com/image/i/products/57/1.jpg", "https://dummyjson.com/image/i/products/57/2.jpg", "https://dummyjson.com/image/i/products/57/3.jpg", "https://dummyjson.com/image/i/products/57/4.jpg", "https://dummyjson.com/image/i/products/57/thumbnail.jpg"
+         ],
+         "sizes": [
+            6, 7, 8, 9
+         ]
+      },
+      {
+         "id": 58,
+         "title": "formal offices shoes",
+         "description": "Pattern Type: Solid, Material: PU, Toe Shape: Pointed Toe ,Outsole Material: Rubber",
+         "price": 57,
+         "discountPercentage": 12,
+         "rating": 4.41,
+         "stock": 68,
+         "brand": "The Warehouse",
+         "category": "mens-shoes",
+         "thumbnail": "https://dummyjson.com/image/i/products/58/thumbnail.jpg",
+         "images": [
+            "https://dummyjson.com/image/i/products/58/1.jpg", "https://dummyjson.com/image/i/products/58/2.jpg", "https://dummyjson.com/image/i/products/58/3.jpg", "https://dummyjson.com/image/i/products/58/4.jpg", "https://dummyjson.com/image/i/products/58/thumbnail.jpg"
+         ],
+         "sizes": [
+            8, 9, 10
+         ]
+      },
+      {
+         "id": 59,
+         "title": "Spring & ' < > summer shoes",
+         "description": "Comfortable stretch cloth, lightweight body; ,rubber sole, anti-skid wear;",
+         "price": 20,
+         "discountPercentage": 8.71,
+         "rating": 4.33,
+         "stock": 137,
+         "brand": "Sneakers",
+         "category": "mens-shoes",
+         "thumbnail": "https://dummyjson.com/image/i/products/59/thumbnail.jpg",
+         "images": [
+            "https://dummyjson.com/image/i/products/59/1.jpg", "https://dummyjson.com/image/i/products/59/2.jpg", "https://dummyjson.com/image/i/products/59/3.jpg", "https://dummyjson.com/image/i/products/59/4.jpg", "https://dummyjson.com/image/i/products/59/thumbnail.jpg"
+         ],
+         "sizes": [
+            11
+         ]
+      },
+      {
+         "id": 60,
+         "title": "Stylish Casual Jeans Shoes",
+         "description": "High Quality ,Stylish design ,Comfortable wear ,Fashion ,Durable",
+         "price": 58,
+         "discountPercentage": 7.55,
+         "rating": 4.55,
+         "stock": 129,
+         "brand": "Sneakers",
+         "category": "mens-shoes",
+         "thumbnail": "https://dummyjson.com/image/i/products/60/thumbnail.jpg",
+         "images": [
+            "https://dummyjson.com/image/i/products/60/1.jpg", "https://dummyjson.com/image/i/products/60/2.jpg", "https://dummyjson.com/image/i/products/60/3.jpg", "https://dummyjson.com/image/i/products/60/thumbnail.jpg"
+         ],
+         "sizes": [
+            10, 11
+         ]
+      }
+   ],
+   "total": 5,
+   "skip": 0,
+   "limit": 5
+};
