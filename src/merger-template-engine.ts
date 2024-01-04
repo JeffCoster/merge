51f8235b590cda2d8fedfb464927243457501556
Jeff Coster <122172619+JeffCoster@users.noreply.github.com>
2024-01-04 17:57:01 +0000

    stop using includeNodeLocations for JSDOM

--- src/merger-template-engine.ts
+++ src/merger-template-engine.ts
@@ -40,9 +40,7 @@ export function __express(filePath: string, options: any, callback: (msg: any, r
         const mapFileDir: string = path.dirname(filePath);
         process.chdir(mapFileDir);
 
-        var dom = JSDOM.fromFile(htmlTemplatePath, {
-        includeNodeLocations: true
-        }).then(dom => {
+        var dom = JSDOM.fromFile(htmlTemplatePath).then(dom => {
             if(debug) {
                 validateMergeMapToSchema(mergeMap4View);
             }
